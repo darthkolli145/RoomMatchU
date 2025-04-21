f83d97f486d6a51abea38a823224c8d36056cfeb
ajaysuryask <ajsenthi@ucsc.edu>
2025-04-21 16:13:51 -0700

    firebase fetching

--- RoomMatchU/src/firebase/firebaseHelpers.js
+++ RoomMatchU/src/firebase/firebaseHelpers.js
@@ -1,4 +1,19 @@
+import { collection, getDocs, addDoc } from "firebase/firestore";
+import { db } from "./config";
 
 // post listing
+// post listings to a new collection in cloud firestore
 
-// fetch listings
+// FETCH all listings
+export const fetchListings = async () => {
+  try {
+    const snapshot = await getDocs(collection(db, "listings"));
+    return snapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data()
+    }));
+  } catch (error) {
+    console.error("Error fetching listings:", error);
+    throw error;
+  }
+};
