04e2e6c2edc4feb161915c909a67612331d355ef
maxwell vongnakhone <movongna@ucsc.edu>
2025-04-21 17:45:45 -0700

    Added firestore fuction to post new listings (backend to add a new listing)

--- RoomMatchU/src/firebase/firebaseHelpers.js
+++ RoomMatchU/src/firebase/firebaseHelpers.js
@@ -1,8 +1,29 @@
 import { collection, getDocs, addDoc } from "firebase/firestore";
 import { db } from "./config";
 
-// post listing
-// post listings to a new collection in cloud firestore
+// POST a new listing to Firestore
+export const postListing = async (formData) => {
+  // only signed in users can post listing
+  try {
+    const user = auth.currentUser;
+    if (!user) {
+      throw new Error("User not authenticated");
+    }
+    // Copy form data
+    const listingData = {
+      ...formData,
+      posterUID: user.uid, // add userID of poster
+      createdAt: serverTimestamp() // timestamp of creation
+    };
+
+    // add listingData to 'listings' collection in FS
+    const docRef = await addDoc(collection(db, "listings"), listingData);
+    return docRef.id;
+  } catch (error) {
+    console.error("Error posting listing:", error);
+    throw error;
+  }
+};
 
 // FETCH all listings
 export const fetchListings = async () => {
